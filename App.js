72fcf49230e24a238b1d301cb73ecb70a467d5df
Baruch Phillips <baruchphillips@gmail.com>
2020-08-27 18:04:50 -0400

    updates

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WEATHER_API_KEY} from 'react-native-dotenv'
 
 export default function App() {
 
-  const URL = `http://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_API_KEY}&q=New York`
+  const URL = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=New York`
   const [state, setstate] = useState([])
-
-  fetch(URL).then(r => r.json()).then(data => {
-   console.log
-  })
-
+console.log(URL)
+    // have to understand useEffect
+  useEffect(() => {
+    fetch(URL).then(r => r.json()).then(data => {
+      setstate(data)
+     })
+  }, [])
+  console.log(state)
   return (
     <View style={styles.container}>
       <Text>This is App file!</Text>
-  <Text>Currently in NYC the weather is {}</Text>
+  <Text>Currently in {state?.location?.name} the weather is {state?.current?.temp_f} degrees and {state?.current?.condition?.text}  </Text>
       <StatusBar style="auto" />
     </View>
   );
